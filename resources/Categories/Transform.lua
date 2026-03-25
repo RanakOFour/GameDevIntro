337ba288bb8846b1b0cae2b3a5f54f62ee786785
Ranak04 <4c3quack@protonmail.com>
2026-03-25 13:55:14 +0000

    Category reloading not working

--- resources/Categories/Transform.lua
+++ resources/Categories/Transform.lua
@@ -3,8 +3,7 @@ local baseAttributes = {
     Position = Vector2.new(0.0),
     Layer = 0,
     Rotation = 0.0,
-    Scale = Vector2.new(1),
-    Test = 0
+    Scale = Vector2.new(1)
 }
 
 return Category.new(categoryName, baseAttributes)
